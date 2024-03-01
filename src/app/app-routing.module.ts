ee2129a31882009a081e51546cc9ff14d6c11102
Likith <K14141@ktkbankltd.com>
2024-03-01 16:39:44 +0530

    code changes details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { DetailsComponent } from './details/details.component';
+import { MainComponent } from './main/main.component';
 
-
-const routes: Routes = [];
-
+const routes: Routes = [
+  // {
+  //   path: '',
+  //   redirectTo: '/main',
+  //   pathMatch :'full'
+  // },
+  {
+    path:'main',
+    component:MainComponent
+  },
+  {
+    path:'details',
+    component: DetailsComponent
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import routermodule manually
+import { RouterModule,Routes } from '@angular/router';
+import { DetailsComponent } from './details/details.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailsComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
